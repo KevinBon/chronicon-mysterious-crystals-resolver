1ad385f096f4d86cb0e373a5a0287fd76b72c481
kevin bon <bon.kevin@gmail.com>
2020-09-02 23:49:00 +0100

    Updates

---
+++ precache-manifest.fc903c4c976dbf1207a67adf71c7bbb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4c4debf710cc9cff518a90409742a50",
+    "url": "/chronicon-mysterious-crystal-resolver/index.html"
+  },
+  {
+    "revision": "b226e0539fbbaab58e2d",
+    "url": "/chronicon-mysterious-crystal-resolver/static/css/main.145295da.chunk.css"
+  },
+  {
+    "revision": "2e62bf0a131c0e512c87",
+    "url": "/chronicon-mysterious-crystal-resolver/static/js/2.1fb06126.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chronicon-mysterious-crystal-resolver/static/js/2.1fb06126.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b226e0539fbbaab58e2d",
+    "url": "/chronicon-mysterious-crystal-resolver/static/js/main.b74444a2.chunk.js"
+  },
+  {
+    "revision": "4a5134fb5ec7026196bf",
+    "url": "/chronicon-mysterious-crystal-resolver/static/js/runtime-main.f26bd38d.js"
+  }
+]);
